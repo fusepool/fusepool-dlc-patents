ff79127f0859b51992cd9d497feb58e2ed29a8e8
Luigi Selmi <selmi_luigi@hotmail.com>
2014-02-24 10:42:33 +0100

    handle sax exception

--- src/main/java/eu/fusepool/rdfizer/marec/xslt/XMLProcessor.java
+++ src/main/java/eu/fusepool/rdfizer/marec/xslt/XMLProcessor.java
@@ -3,11 +3,13 @@ package eu.fusepool.rdfizer.marec.xslt;
 import java.io.InputStream;
 import javax.xml.transform.TransformerException;
 
+import org.xml.sax.SAXNotRecognizedException;
+
 /**
  * Transforms an InputStream in a specific XML format to an RDF/XML Stream
  */
 public interface XMLProcessor {
 
-    public InputStream processXML(InputStream is) throws TransformerException;
+    public InputStream processXML(InputStream is) throws SAXNotRecognizedException, TransformerException;
 
 }
